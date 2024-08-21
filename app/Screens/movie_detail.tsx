92be32cca4f42a57063a93f40b0d0ffdef0cc311
Thiha Aung <leothihaaung@gmail.com>
2024-08-21 15:07:55 +0700

    back button and title change

--- app/Screens/movie_detail.tsx
+++ app/Screens/movie_detail.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const MovieDetailScreen = () => {
+  const navigation = useNavigation();
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Movie Details', // Custom title
+      headerBackTitle: 'Back', // Custom back button label
+    });
+  }, [navigation]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <Text>Movie Detail..</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Movie Detail Screen</Text>
     </View>
   );
 };
