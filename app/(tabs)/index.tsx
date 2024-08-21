d3941f748a00b447d078578ac611ad7e9a5b36df
Thiha Aung <leothihaaung@gmail.com>
2024-08-21 15:05:46 +0700

    button style

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'expo-router';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Pressable, Text, View} from 'react-native';
 
 const PopularMoviesScreen = () => {
   return (
@@ -11,9 +11,33 @@ const PopularMoviesScreen = () => {
         alignItems: 'center',
       }}>
       
-      <Link href='/screens/movie_detail'>Explore</Link>
+      <Link href="/screens/movie_detail" asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.text}>Movie Detail</Text>
+        </Pressable>
+      </Link>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'blue',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
+
 export default PopularMoviesScreen;
+
