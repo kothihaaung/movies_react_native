2b883194947280ff511abcbbe4b3e77ef0cfe4a1
Thiha Aung <leothihaaung@gmail.com>
2024-08-22 13:26:18 +0700

    initial movie detail screen

--- app/features/movies/screens/movie_detail.tsx
+++ app/features/movies/screens/movie_detail.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View, StyleSheet, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useLocalSearchParams } from 'expo-router';
 import { Movie } from '../movie';
+import { router } from 'expo-router';
 
 const MovieDetailScreen = () => {
     const navigation = useNavigation();
@@ -13,19 +14,49 @@ const MovieDetailScreen = () => {
 
     const { item } = useLocalSearchParams<Params>();
     const movie: Movie = JSON.parse(item)
-    
+
     React.useLayoutEffect(() => {
         navigation.setOptions({
-        title: movie.title, // Custom title
-        headerBackTitle: 'Back', // Custom back button label
+            headerShown: false // Custom back button label
         });
     }, [navigation]);
 
+    const onPress = () => {
+        router.back()
+    };
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Movie Detail Screen</Text>
+        <View style={styles.container}>
+            <Button title='Back' onPress={onPress} />
+            <Image
+                source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+                style={styles.poster}
+            />
+
+            <Text style={styles.title}>{movie.title}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 20,
+      backgroundColor: '#222',
+    },
+    poster: {
+      width: 300,
+      height: 450,
+      borderRadius: 8,
+    },
+    title: {
+      color: 'white',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 20,
+    },
+  });
 
 export default MovieDetailScreen;
